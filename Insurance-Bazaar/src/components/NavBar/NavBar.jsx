01489fd400640094d864a4be95d42917f1d9a6bd
Ashique <ashique9496@gmail.com>
2024-12-12 11:45:42 +0530

    navbar desktop view completed

--- Insurance-Bazaar/src/components/NavBar/NavBar.jsx
+++ Insurance-Bazaar/src/components/NavBar/NavBar.jsx
@@ -1,37 +1,61 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./NavBar.scss";
 
 const NavBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-transparent nav">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div className="navbars">
+            <div className="container-fluid">
+                <div className="row nav_row">
+                    <div className="col-xl-2 col-lg-2 col-md-6 nav_col1">
+                        <div className="logo">
+                            <a href="">
+                                LOGO
+                                <img src="" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-8 d-none d-lg-block nav_col2">
+                        <div className="nav_menu">
+                            <div id="mobile_menu">
+                                <ul>
+                                    <li>
+                                        <a href="">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="">service</a>
+                                    </li>
+                                    <li>
+                                        <a href="">companies</a>
+                                    </li>
+                                    <li>
+                                        <a href="">About us</a>
+                                    </li>
+                                    <li>
+                                        <a href="">privacy policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-4 col-lg-2 col-md-6 nav_col3">
+                        <div className="items">
+                            <div className="call_us">
+                                <i className="bx bxs-phone-call"></i>
+                                <p>+91 1234567890</p>
+                            </div>
+                            <div className="nav_button">
+                                <button>get started</button>
+                            </div>
+                            <div className="mobile_icon">
+                                <a href="">
+                                    <i className="bx bx-menu"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
